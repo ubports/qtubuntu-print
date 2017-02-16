7489e497318a7f3c7a5051cc47d128cb256dd74d
Andrew Hayzen <ahayzen@gmail.com>
2017-02-16 14:45:11 +0000

    * Change to using LGPL instead of GPL

--- qubuntuprintsupport_p.h
+++ qubuntuprintsupport_p.h
@@ -4,16 +4,16 @@
  * This file is part of qtubuntu-print.
  *
  * qtubuntu-print is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; version 3.
  *
  * qtubuntu-print is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with qtubuntu-print.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored-by: Andrew Hayzen <andrew.hayzen@canonical.com>
  */
